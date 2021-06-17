15818f04070d07bacf948ff9ebab14b398e3921e
Karla-Ortiz-Chávez-2018630022 <75957935+KarlaOrtiz24@users.noreply.github.com>
2021-06-16 22:38:58 -0500

    Delete ImplementacionElementos.java

--- Prácticas/ImplementacionElementos.java
+++
@@ -1,9 +0,0 @@
-public class ImplementacionElementos {
-    public String elementos; 
-    public void ImplementacionElementos(String elementos) { 
-        System.out.println("Elemento: " + elementos); 
-    }/** 
-    public int Iguales(elementos x, elementos y) { 
-        return x == y;
-    }*/
-}
